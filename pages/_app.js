73fd88e1932d03eb84ad7698cba6ca15e9e592fe
Ayushi Prakash <ayushiprakash03@gmail.com>
2021-05-15 20:51:11 +0530

    💄 Add ThemeProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ThemeProvider } from "styled-components";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider
+      theme={{
+        textPrimary: "#2C2C2C",
+        textSecondary:  "#666",
+        accentColor: "#ff503f",
+        backgroundPrimary: "#F4F4F2",
+        backgroundSecondary: "#D4C9C9",
+      }}
+    >
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
