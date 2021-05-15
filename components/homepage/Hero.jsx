dd49e8a7b7a8a5f0e71a868df51bf426606e6fa7
Ayushi Prakash <ayushiprakash03@gmail.com>
2021-05-15 20:11:57 +0530

    Init commit

---
+++ components/homepage/Hero.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Header } from "../shared";
+
+const Hero = () => {
+  return <div></div>;
+};
+
+export default Hero;
---
+++ components/homepage/index.jsx
@@ -0,0 +1,5 @@
+export { default as Hero } from "./Hero";
+export { default as About } from "./About";
+export { default as Contact } from "./Contact";
+export { default as Experience } from "./Experience";
+export { default as Projects } from "./Projects";
