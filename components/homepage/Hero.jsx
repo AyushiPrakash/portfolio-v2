e1127bb3bd1f2207aa9608a65dcfcaaf4140bca3
Ayushi Prakash <ayushiprakash03@gmail.com>
2021-06-14 00:02:32 +0530

    📱 Responsive Hero Page

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -5,25 +5,42 @@ import { Title, Header } from "../shared/";
 const Container = styled.main`
   min-height: 100vh;
   background-color: ${(props) => props.theme.backgroundSecondary};
-`;
 
-const LeftSection = styled.section`
-  width: 100%;
-  max-width: 1200px;
-  margin: 0 auto;
-  height: 50%;
-  margin: auto;
-  position: absolute;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
+  .leftSection {
+    width: 100%;
+    max-width: calc(1200px + 4rem);
+    margin: 0 auto;
+    padding: 0 2rem;
+    height: 50%;
+    margin: auto;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
 
-  .info {
-    font-size: 52pt;
-    font-weight: 600;
-    font-family: "Blinker", sans-serif;
-    color: #2c2c2c;
+    .info {
+      font-size: 52pt;
+      font-weight: 600;
+      font-family: "Blinker", sans-serif;
+      color: #2c2c2c;
+    }
+  }
+
+  @media only screen and (max-width: 560px) {
+    .leftSection {
+      .info {
+        font-size: 48pt;
+      }
+    }
+  }
+
+  @media only screen and (max-width: 420px) {
+    .leftSection {
+      .info {
+        font-size: 36pt;
+      }
+    }
   }
 `;
 
@@ -31,13 +48,13 @@ const Hero = () => {
   return (
     <Container>
       <Header />
-      <LeftSection>
+      <div class="leftSection">
         <Title color={"#f4f4f2"}>Ayushi Prakash</Title>
         <div className="info">
           Hello, my <br /> name's Ayushi.
           <br /> I'm a developer & <br /> a designer.
         </div>
-      </LeftSection>
+      </div>
     </Container>
   );
 };
