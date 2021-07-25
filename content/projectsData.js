18a3b93173ca0523f796e461e304011b1e5026ca
Ayushi Prakash <ayushiprakash03@gmail.com>
2021-07-26 01:45:18 +0530

    📝 Add content

--- content/projectsData.js
+++ content/projectsData.js
@@ -1,38 +1,24 @@
 export const ProjectsData = [{
         name: "BWI Website Builder ",
-        description: "- A website for getting paid web development orders from clients around the world.  \n",
-        tools: ["React", "Firebase", "Axios", "MaterialUI"],
+        description: "- A website for getting paid web development orders from clients around the world. \n - Clients can specify the name of the website, their requirements, a preferred color-scheme and a logo. Upon initial fee payment they will be redirected to Build with Innovation's WhatsApp handle to contact the developers.",
+        tools: ["React", "Firebase", "Razorpay", "MaterialUI", "i18n"],
         githubLink: null,
         demoLink: "https://bwi-website-builder.web.app/",
     },
     {
         name: "Twitter Clone",
-        description: "A functional Twitter Clone with Auth System and CRUD API.",
-        tools: ["Javascript", "Expressjs", "CSS", "EJS", "SQL"],
+        description: "A functional Twitter Clone with Auth System and CRUD API. Deployment in Progress.",
+        tools: ["Javascript", "Expressjs", "CSS", "EJS", "SQL", "JWT"],
         githubLink: "https://github.com/AyushiPrakash/Twitter-Clone",
         demoLink: null,
     },
 
     {
         name: "Misty, A weather app",
-        description: "Get the current weather for any location on the fly.",
+        description: "- Get the current weather for any location on the fly. \n - Made with Reactjs to learn about API calls, third-part API keys, and the useEffect hooks.",
         tools: ["React", "Axios"],
         githubLink: "https://github.com/AyushiPrakash/Misty",
         demoLink: "https://misty.vercel.app/",
-    },
-    {
-        name: "Life Checklist",
-        description: null,
-        tools: ["React", "SASS"],
-        githubLink: "https://github.com/AyushiPrakash/Life-Checklist",
-        demoLink: "https://lifechecklist.vercel.app/",
-    },
-    {
-        name: "Bulletin Board",
-        description: "A simple app to write anything on the board.",
-        tools: ["React"],
-        githubLink: "https://github.com/AyushiPrakash/Bulletin-Board",
-        demoLink: "https://pinboard-jn2cadotw.vercel.app/",
     },
     {
         name: "To-do List",
@@ -40,5 +26,19 @@ export const ProjectsData = [{
         tools: ["HTML", "CSS", "Javascript"],
         githubLink: "https://github.com/AyushiPrakash/TO-DO-List",
         demoLink: "https://ayushiprakash.github.io/TO-DO-List/",
+    }, ,
+    {
+        name: "Bulletin Board",
+        description: "A unique note-making/organizing app to take textual notes and arrange them on your board. Bulletin board respects your priorities.",
+        tools: ["React", "React-draggable"],
+        githubLink: "https://github.com/AyushiPrakash/Bulletin-Board",
+        demoLink: "https://pinboard-jn2cadotw.vercel.app/",
+    },
+    {
+        name: "Life Checklist",
+        description: "- A frontend only webapp to keep track of your major life events.\n - All the data is stored on localstorage, so you dont have to worry about privacy issues 😉.",
+        tools: ["React", "SASS"],
+        githubLink: "https://github.com/AyushiPrakash/Life-Checklist",
+        demoLink: "https://lifechecklist.vercel.app/",
     },
 ];
